c4f659d068f268072f32a89429694236fee811fd
Niccolo Brogi <me@nbrogi.com>
2018-02-24 11:11:28 +0100

    Fix whitespace

--- src/parallax/ParallaxLandscape.js
+++ src/parallax/ParallaxLandscape.js
@@ -17,204 +17,203 @@ import back02 from '../images/back-02.png';
 
 
 class ParallaxLandscape extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            stage: 0,
-        };
-    }
-
-    handleClickNext = () => {
-        this.setState(this.state.stage >= 2 ? {stage:0}:{stage:this.state.stage+1})
+  constructor(props) {
+    super(props);
+    this.state = {
+      stage: 0,
     };
-    handleClickPrev = () => {
-        this.setState(this.state.stage <= 0 ? {stage:2}:{stage:this.state.stage-1})
+  }
+
+  handleClickNext = () => {
+    this.setState(this.state.stage >= 2 ? {stage:0}:{stage:this.state.stage+1})
+  };
+  handleClickPrev = () => {
+    this.setState(this.state.stage <= 0 ? {stage:2}:{stage:this.state.stage-1})
+  };
+
+  render() {
+    let LandscapeStyle = {
+      width: this.props.visibleX,
+      height: this.props.visibleY,
+      position: 'relative',
+      overflow: 'hidden',
     };
 
 
-    render() {
-        let LandscapeStyle = {
-            width: this.props.visibleX,
-            height: this.props.visibleY,
-            position: 'relative',
-            overflow: 'hidden',
-        };
+    return (
+      <div className="ParallaxLandscape">
 
+        <div className="buttonContainer">
+          <button className="btn" name="Prev" onClick={this.handleClickPrev}  > Prev </button>
+          <button className="btn" name="Next" onClick={this.handleClickNext}  > Next </button>
+        </div>
 
-        return (
-            <div className="ParallaxLandscape">
+        <div style={LandscapeStyle}>
+          <ParallaxLandscapeLayer
+            width={this.props.width}
+            height={this.props.height}
+            scrollX={this.props.visibleX}  // amount to move per stage
+            stage={this.state.stage}    // stage of animation
+            layer_name="grass"     // layer name
+            backgroundRepeat="repeat-x" // for css purposes setting "background-repeat" value
+            img={grass13}   // image
+            x={0}       // starting position x only numbers(pixels), will parse string into number
+            y={50}     // starting position y can be in string = "10%"
+            move={false}  // can be false, right, left
+            loop={false}  // when the object moves outside the very end, should it appear again on the other side?
+            speed="0"     // speed px per 50ms only for moving objects
+            level={1}     // 1 = foreground, pixel amount to move on click is dependant on it
+          />
 
-                <div className="buttonContainer">
-                    <button className="btn" name="Prev" onClick={this.handleClickPrev}  > Prev </button>
-                    <button className="btn" name="Next" onClick={this.handleClickNext}  > Next </button>
-                </div>
+          <ParallaxLandscapeLayer
+            width={this.props.width}
+            height={this.props.height}
+            scrollX={this.props.visibleX}
+            stage={this.state.stage}
+            layer_name="street"
+            backgroundRepeat="repeat-x"
+            img={street12}
+            x={0}
+            y={-614}
+            move="false"
+            loop={false}
+            speed="0"
+            level={1.2}
+          />
 
-                <div style={LandscapeStyle}>
-                    <ParallaxLandscapeLayer
-                        width={this.props.width}
-                        height={this.props.height}
-                        scrollX={this.props.visibleX}    // amount to move per stage
-                        stage={this.state.stage}        // stage of animation
-                        layer_name="grass"         // layer name
-                        backgroundRepeat="repeat-x" // for css purposes setting "background-repeat" value
-                        img={grass13}     // image
-                        x={0}           // starting position x only numbers(pixels), will parse string into number
-                        y={50}         // starting position y can be in string = "10%"
-                        move={false}    // can be false, right, left
-                        loop={false}    // when the object moves outside the very end, should it appear again on the other side?
-                        speed="0"       // speed px per 50ms only for moving objects
-                        level={1}       // 1 = foreground, pixel amount to move on click is dependant on it
-                    />
+          <ParallaxLandscapeLayer
+            width={this.props.width}
+            height={this.props.height}
+            scrollX={this.props.visibleX}
+            stage={this.state.stage}
+            layer_name="front1"
+            backgroundRepeat="repeat-x"
+            img={front01}
+            x={0}
+            y={0}
+            move="false"
+            loop={false}
+            speed="0"
+            level={1.4}
+          />
 
-                    <ParallaxLandscapeLayer
-                        width={this.props.width}
-                        height={this.props.height}
-                        scrollX={this.props.visibleX}
-                        stage={this.state.stage}
-                        layer_name="street"
-                        backgroundRepeat="repeat-x"
-                        img={street12}
-                        x={0}
-                        y={-614}
-                        move="false"
-                        loop={false}
-                        speed="0"
-                        level={1.2}
-                    />
+          <ParallaxLandscapeLayer
+            width={this.props.width}
+            height={this.props.height}
+            scrollX={this.props.visibleX}
+            stage={this.state.stage}
+            layer_name="front2"
+            backgroundRepeat="repeat-x"
+            img={front02}
+            x={0}
+            y={0}
+            move="false"
+            loop={false}
+            speed="0"
+            level={1.5}
+          />
 
-                    <ParallaxLandscapeLayer
-                        width={this.props.width}
-                        height={this.props.height}
-                        scrollX={this.props.visibleX}
-                        stage={this.state.stage}
-                        layer_name="front1"
-                        backgroundRepeat="repeat-x"
-                        img={front01}
-                        x={0}
-                        y={0}
-                        move="false"
-                        loop={false}
-                        speed="0"
-                        level={1.4}
-                    />
+          <ParallaxLandscapeLayer
+            width={this.props.width}
+            height={this.props.height}
+            scrollX={this.props.visibleX}
+            stage={this.state.stage}
+            layer_name="plane"
+            backgroundRepeat="no-repeat"
+            img={plane}
+            x={this.props.visibleX-50}  // to start plane from visible state
+            y="5%"
+            move="left"
+            loop={true}
+            speed="1"
+            level={2}
+          />
 
-                    <ParallaxLandscapeLayer
-                        width={this.props.width}
-                        height={this.props.height}
-                        scrollX={this.props.visibleX}
-                        stage={this.state.stage}
-                        layer_name="front2"
-                        backgroundRepeat="repeat-x"
-                        img={front02}
-                        x={0}
-                        y={0}
-                        move="false"
-                        loop={false}
-                        speed="0"
-                        level={1.5}
-                    />
+          <ParallaxLandscapeLayer
+            width={this.props.width}
+            height={this.props.height}
+            scrollX={this.props.visibleX}
+            stage={this.state.stage}
+            layer_name="boat"
+            backgroundRepeat="no-repeat"
+            img={fishingboat}
+            x={0}
+            y="30%"
+            move="right"
+            loop={true}
+            speed="0.5"
+            level={2.5}
+          />
 
-                    <ParallaxLandscapeLayer
-                        width={this.props.width}
-                        height={this.props.height}
-                        scrollX={this.props.visibleX}
-                        stage={this.state.stage}
-                        layer_name="plane"
-                        backgroundRepeat="no-repeat"
-                        img={plane}
-                        x={this.props.visibleX-50}  // to start plane from visible state
-                        y="5%"
-                        move="left"
-                        loop={true}
-                        speed="1"
-                        level={2}
-                    />
+          <ParallaxLandscapeLayer
+            width={this.props.width}
+            height={this.props.height}
+            scrollX={this.props.visibleX}
+            stage={this.state.stage}
+            layer_name="hill"
+            backgroundRepeat="repeat-x"
+            img={back}
+            x={0}
+            y={0}
+            move={false}
+            loop={false}
+            speed="0"
+            level={3}
+          />
 
-                    <ParallaxLandscapeLayer
-                        width={this.props.width}
-                        height={this.props.height}
-                        scrollX={this.props.visibleX}
-                        stage={this.state.stage}
-                        layer_name="boat"
-                        backgroundRepeat="no-repeat"
-                        img={fishingboat}
-                        x={0}
-                        y="30%"
-                        move="right"
-                        loop={true}
-                        speed="0.5"
-                        level={2.5}
-                    />
+          <ParallaxLandscapeLayer
+            width={this.props.width}
+            height={this.props.height}
+            scrollX={this.props.visibleX}
+            stage={this.state.stage}
+            layer_name="hill2"
+            backgroundRepeat="repeat-x"
+            img={back02}
+            x={-1500}
+            y={0}
+            move={false}
+            loop={false}
+            speed="0"
+            level={3}
+          />
 
-                    <ParallaxLandscapeLayer
-                        width={this.props.width}
-                        height={this.props.height}
-                        scrollX={this.props.visibleX}
-                        stage={this.state.stage}
-                        layer_name="hill"
-                        backgroundRepeat="repeat-x"
-                        img={back}
-                        x={0}
-                        y={0}
-                        move={false}
-                        loop={false}
-                        speed="0"
-                        level={3}
-                    />
+          <ParallaxLandscapeLayer
+            width={this.props.width}
+            height={this.props.height}
+            scrollX={this.props.visibleX}
+            stage={this.state.stage}
+            layer_name="water"
+            backgroundRepeat="repeat-x"
+            img={water}
+            x={0}
+            y={0}
+            move="false"
+            loop={false}
+            speed="0"
+            level={3.5}
+          />
 
-                    <ParallaxLandscapeLayer
-                        width={this.props.width}
-                        height={this.props.height}
-                        scrollX={this.props.visibleX}
-                        stage={this.state.stage}
-                        layer_name="hill2"
-                        backgroundRepeat="repeat-x"
-                        img={back02}
-                        x={-1500}
-                        y={0}
-                        move={false}
-                        loop={false}
-                        speed="0"
-                        level={3}
-                    />
+          <ParallaxLandscapeLayer
+            width={this.props.width}
+            height={this.props.height}
+            scrollX={this.props.visibleX}
+            stage={this.state.stage}
+            layer_name="clouds"
+            backgroundRepeat="repeat-x"
+            img={clouds}
+            x={0}
+            y={0}
+            move="false"
+            loop={false}
+            speed="0"
+            level={4}
+          />
 
-                    <ParallaxLandscapeLayer
-                        width={this.props.width}
-                        height={this.props.height}
-                        scrollX={this.props.visibleX}
-                        stage={this.state.stage}
-                        layer_name="water"
-                        backgroundRepeat="repeat-x"
-                        img={water}
-                        x={0}
-                        y={0}
-                        move="false"
-                        loop={false}
-                        speed="0"
-                        level={3.5}
-                    />
+        </div>
 
-                    <ParallaxLandscapeLayer
-                        width={this.props.width}
-                        height={this.props.height}
-                        scrollX={this.props.visibleX}
-                        stage={this.state.stage}
-                        layer_name="clouds"
-                        backgroundRepeat="repeat-x"
-                        img={clouds}
-                        x={0}
-                        y={0}
-                        move="false"
-                        loop={false}
-                        speed="0"
-                        level={4}
-                    />
-
-                </div>
-
-            </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 
 export default ParallaxLandscape;
--- src/parallax/ParallaxLandscapeLayer.js
+++ src/parallax/ParallaxLandscapeLayer.js
@@ -2,61 +2,59 @@ import React, { Component } from 'react';
 import {Motion, spring} from 'react-motion';
 
 class ParallaxLandscapeLayer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            posx: parseFloat(this.props.x),
-            repeat: true,
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      posx: parseFloat(this.props.x),
+      repeat: true,
+    };
 
-        if (this.props.move !== false)
-            setInterval(() => {
-                if (this.state.posx > this.props.width){
-                    this.setState({posx: 0, repeat: this.props.loop})
-                }
-                if (this.state.posx < 0){
-                    this.setState({posx: this.props.width, repeat: this.props.loop})
-                }
-                if (this.props.move === 'right' && this.state.repeat === true){
-                    this.setState(previousState => {
-                        return { posx: previousState.posx + parseFloat(this.props.speed) };
-                    });
-                }
-                if (this.props.move === 'left' && this.state.repeat === true ){
-                    this.setState(previousState => {
-                        return { posx: previousState.posx - parseFloat(this.props.speed) };
-                    });
-                }
+    if (this.props.move !== false)
+      setInterval(() => {
+        if (this.state.posx > this.props.width){
+          this.setState({posx: 0, repeat: this.props.loop})
+        }
+        if (this.state.posx < 0){
+          this.setState({posx: this.props.width, repeat: this.props.loop})
+        }
+        if (this.props.move === 'right' && this.state.repeat === true){
+          this.setState(previousState => {
+            return { posx: previousState.posx + parseFloat(this.props.speed) };
+          });
+        }
+        if (this.props.move === 'left' && this.state.repeat === true ){
+          this.setState(previousState => {
+            return { posx: previousState.posx - parseFloat(this.props.speed) };
+          });
+        }
 
-            }, 50);
-    }
+      }, 50);
+  }
 
+  render() {
+    return (
+      <div>
+        <Motion style={{x: spring(this.props.stage*this.props.scrollX,{stiffness: 60, damping: 15,})}}>
+          {({x}) =>
+            <div className={this.props.layer_name} style={{
+              position: 'absolute',
+              background: 'url('+this.props.img+') ',
+              backgroundRepeat: this.props.backgroundRepeat,
+              width: this.props.width,
+              height: this.props.height,
+              zIndex: this.props.level*(-10),
+              top: this.props.y,
+              left: this.state.posx,
+              WebkitTransform: `translateX(${-x/this.props.level}px)`,
+              transform: `translateX(${-x/this.props.level}px)`,
 
-    render() {
+            }}/>
+          }
+        </Motion>
 
-        return (
-            <div>
-                <Motion style={{x: spring(this.props.stage*this.props.scrollX,{stiffness: 60, damping: 15,})}}>
-                    {({x}) =>
-                        <div className={this.props.layer_name} style={{
-                            position: 'absolute',
-                            background: 'url('+this.props.img+') ',
-                            backgroundRepeat: this.props.backgroundRepeat,
-                            width: this.props.width,
-                            height: this.props.height,
-                            zIndex: this.props.level*(-10),
-                            top: this.props.y,
-                            left: this.state.posx,
-                            WebkitTransform: `translateX(${-x/this.props.level}px)`,
-                            transform: `translateX(${-x/this.props.level}px)`,
-
-                        }}/>
-                    }
-                </Motion>
-
-            </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 
 export default ParallaxLandscapeLayer;
